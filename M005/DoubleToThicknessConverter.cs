a73dbcba0cd6395ad5a3b6dbd03da3e24fbc9e23
ppedvLukasK <lukask@ppedv.de>
2023-10-10 09:56:56 +0200

    M005

---
+++ M005/DoubleToThicknessConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace M005
+{
+	class DoubleToThicknessConverter : IValueConverter
+	{
+		//Source -> Target
+		//Slider -> Button (möglich)
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			//Der value Parameter enthält den Wert der beim Binding gesendet wird
+			double sliderValue = (double) value;
+			return new Thickness(sliderValue);
+		}
+
+		//Target -> Source
+		//Button -> Slider (nicht möglich)
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ M005/FourValueToThicknessConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace M005
+{
+	class FourValueToThicknessConverter : IMultiValueConverter
+	{
+		public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+		{
+			double l = (double) values[0];
+			double t = (double) values[1];
+			double r = (double) values[2];
+			double b = (double) values[3];
+			return new Thickness(l, t, r, b);
+		}
+
+		public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
