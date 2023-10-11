51f7277a8d9140bb9f14638da646d4b4143defd0
ppedvLukasK <lukask@ppedv.de>
2023-10-11 14:34:59 +0200

    M010

---
+++ M010/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace M010;
+
+public partial class MainWindow : Window
+{
+	public List<string> Personen { get; set; }
+
+	public bool PersonenNichtLeer => CB.HasItems;
+
+	public MainWindow()
+	{
+		InitializeComponent();
+		CB.ItemsSource = Personen;
+	}
+
+	private void Button_Click(object sender, RoutedEventArgs e)
+	{
+		CB.Items.Add("123");
+	}
+}
