bf7b1c5aa8ef5d537b54ccda02aa3e0eb7021e09
ppedvLukasK <lukask@ppedv.de>
2023-10-11 09:31:49 +0200

    M008

---
+++ M008/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace M008
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			LB.ItemsSource = new List<string>() { "L1", "L2", "L3" };
+			CB.ItemsSource = new List<Person>()
+			{
+				new Person(){ Id = 0, Name = "Max", Description = "Person1" },
+				new Person(){ Id = 1, Name = "Max2", Description = "Person2" },
+				new Person(){ Id = 2, Name = "Max3", Description = "Person3" }
+			};
+
+			PersonLB.ItemsSource = CB.ItemsSource;
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			Close();
+		}
+	}
+}
---
+++ M008/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace M008;
+
+internal class Person
+{
+	public int Id { get; set; }
+
+	public string Name { get; set; }
+
+	public string Description { get; set; }
+}
