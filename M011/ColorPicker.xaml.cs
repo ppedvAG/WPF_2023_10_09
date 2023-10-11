b73032435ca48f90fc69f85cbc0d765879e799c4
ppedvLukasK <lukask@ppedv.de>
2023-10-11 15:55:41 +0200

    M011

---
+++ M011/ColorPicker.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace M011;
+
+public partial class ColorPicker : UserControl
+{
+	public Color PickedColor
+	{
+		get { return (Color) GetValue(PickedColorProperty); }
+		set { SetValue(PickedColorProperty, value); }
+	}
+
+	/// <summary>
+	/// Über ein DependencyProperty können bei UserControls eigene Properties weitergegeben werden
+	/// </summary>
+	public static readonly DependencyProperty PickedColorProperty =
+		DependencyProperty.Register(
+			"PickedColor", //Name des Felds
+			typeof(Color), //Typ des Felds
+			typeof(ColorPicker), //Typ des Besitzers
+			new PropertyMetadata(Colors.Transparent)); //Standardwert
+
+	public ColorPicker()
+	{
+		InitializeComponent();
+
+		Binding binding = new("Fill.Color")
+		{
+			Source = Rect,
+			Mode = BindingMode.OneWay
+		};
+		BindingOperations.SetBinding(this, PickedColorProperty, binding);
+	}
+}
---
+++ M011/RGBAToColorConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace M011;
+
+public class RGBAToColorConverter : IMultiValueConverter
+{
+	public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+	{
+		Color col = Color.FromArgb(
+				(byte) (double) values[0],
+				(byte) (double) values[1],
+				(byte) (double) values[2],
+				(byte) (double) values[3]);
+		return new SolidColorBrush(col);
+	}
+
+	public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+	{
+		throw new NotImplementedException();
+	}
+}
